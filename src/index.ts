ff40f950c2fbc8dabb0bf8d3da3acbcc3d2ebf9c
Mat Groves <mat@goodboydigital.com>
2021-05-05 21:30:42 +0100

    renderer setup

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-import { Application } from "@pixi/app";
-import { Texture } from "@pixi/core";
-import { Graphics } from "@pixi/graphics";
-import { Sprite } from "@pixi/sprite";
-import { Ticker } from "@pixi/ticker";
-
-const app = new Application({
-    width:window.innerWidth,
-    height:window.innerHeight,
-    autoStart:true,
-})
+import { Renderer,Container,Graphics,Ticker, Sprite } from "pixi.js";
 
 
 document.body.style.margin = '0px';
-
-document.body.appendChild(app.view);
 document.body.style.overflow = 'hidden';
 
+const renderer = new Renderer({
+    width:window.innerWidth,
+    height:window.innerHeight,
+    
+})
+
+document.body.appendChild(renderer.view);
+
+
+const stage = new Container();
 const g = new Graphics().beginFill(0xFF0000).drawCircle(0,0,100);
 
-const bunny = new Sprite(Texture.WHITE);
-
-bunny.scale.set(100)
-app.stage.addChild(bunny);
-app.stage.addChild(g);
+stage.addChild(g);
 
 Ticker.shared.add(()=>{
-    console.log("M>")
+    renderer.render(stage);
+
+    // do stuff..
+
+
 });
